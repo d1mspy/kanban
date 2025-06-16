8e0fb8bd9f82dd333c8168eeb3d0c0cc3c3f3f91
d1mspy <subbotin2126@gmail.com>
2025-06-16 19:44:13 +0300

    fix queries formatting

--- backend/internal/postgres/queries.go
+++ backend/internal/postgres/queries.go
@@ -4,14 +4,16 @@ const (
 
 	// Creating tables
 
-	QueryCreateUserTable = `CREATE TABLE IF NOT EXISTS "user"(
+	QueryCreateUserTable = `
+		CREATE TABLE IF NOT EXISTS "user"(
 		id uuid PRIMARY KEY,
 		created_at timestamptz NOT NULL,
 		username text NOT NULL UNIQUE,
 		hashed_password text NOT NULL
 	)`
 
-	QueryCreateBoardTable = `CREATE TABLE IF NOT EXISTS board(
+	QueryCreateBoardTable = `
+		CREATE TABLE IF NOT EXISTS board(
 		id uuid PRIMARY KEY,
 		user_id uuid REFERENCES "user"(id) ON DELETE CASCADE,
 		created_at timestamptz NOT NULL,
@@ -19,7 +21,8 @@ const (
 		name text NOT NULL
 	)`
 
-	QueryCreateColumnTable = `CREATE TABLE IF NOT EXISTS "column"(
+	QueryCreateColumnTable = `
+		CREATE TABLE IF NOT EXISTS "column"(
 		id uuid PRIMARY KEY,
 		board_id uuid REFERENCES board(id) ON DELETE CASCADE,
 		created_at timestamptz NOT NULL,
@@ -28,7 +31,8 @@ const (
 		position smallint NOT NULL
 	)`
 
-	QueryCreateTaskTable = `CREATE TABLE IF NOT EXISTS task(
+	QueryCreateTaskTable = `
+		CREATE TABLE IF NOT EXISTS task(
 		id uuid PRIMARY KEY,
 		column_id uuid REFERENCES "column"(id) ON DELETE CASCADE,
 		created_at timestamptz NOT NULL,
@@ -42,15 +46,26 @@ const (
 
 	// Auth queries
 
-	QueryCreateUser = `INSERT INTO "user" (id, username, hashed_password, created_at) VALUES ($1, $2, $3, $4)`
+	QueryCreateUser = `
+		INSERT INTO "user" 
+		(id, username, hashed_password, created_at) 
+		VALUES ($1, $2, $3, $4)`
 
-	QueryGetUserByUsername = `SELECT id, username, hashed_password FROM "user" WHERE username=$1`
+	QueryGetUserByUsername = `
+		SELECT id, username, hashed_password 
+		FROM "user" WHERE username=$1`
 
 	// Board Queries
 
-	QueryCreateBoard = `INSERT INTO board (id, user_id, created_at, updated_at, name) VALUES ($1, $2, $3, $4, $5)`
+	QueryCreateBoard = `
+		INSERT INTO board 
+		(id, user_id, created_at, updated_at, name) 
+		VALUES ($1, $2, $3, $4, $5)`
 
-	QueryGetAllBoards = `SELECT * FROM board WHERE user_id = $1 ORDER BY created_at`
+	QueryGetAllBoards = `
+		SELECT * FROM board 
+		WHERE user_id = $1 
+		ORDER BY created_at`
 
 	QueryGetBoard = `SELECT * FROM board WHERE id = $1`
 
@@ -58,43 +73,69 @@ const (
 		SET updated_at = $1, name = $2
 		WHERE id = $3`
 
-	QueryDeleteBoard = `DELETE FROM board WHERE id = $1`
+	QueryDeleteBoard = `
+		DELETE FROM board 
+		WHERE id = $1`
 
 	// Column queries
 
-	QueryGetMaxColumnPosition = `SELECT COALESCE(MAX(position), 0) + 1 FROM "column" WHERE board_id = $1`
+	QueryGetMaxColumnPosition = `
+		SELECT COALESCE(MAX(position), 0) + 1 
+		FROM "column" 
+		WHERE board_id = $1`
 
-	QueryGetColumnsCount = `SELECT COUNT(*) FROM "column" WHERE board_id = $1`
+	QueryGetColumnsCount = `
+		SELECT COUNT(*) 
+		FROM "column" 
+		WHERE board_id = $1`
 
-	QueryCreateColumn = `INSERT INTO "column" 
+	QueryCreateColumn = `
+		INSERT INTO "column" 
 		(id, board_id, created_at, updated_at, name, position) 
 		VALUES ($1, $2, $3, $4, $5, $6)`
 
-	QueryGetColumn = `SELECT * FROM "column"
+	QueryGetColumn = `
+		SELECT * 
+		FROM "column"
 		WHERE "column".id = $1`
 
-	QueryGetAllColumns = `SELECT * FROM "column"
+	QueryGetAllColumns = `
+		SELECT * 
+		FROM "column"
 		WHERE board_id = $1
 		ORDER BY position`
 	
-	QueryDeleteColumn = `DELETE FROM "column" WHERE id = $1`
+	QueryDeleteColumn = `
+		DELETE FROM "column" 
+		WHERE id = $1`
 	
-	QueryDecreaseColumnsPosition = `UPDATE "column" 
+	QueryDecreaseColumnsPosition = `
+		UPDATE "column" 
 		SET position = position - 1
 		WHERE board_id = $1
 		AND position > $2`
 
-	QueryGetBoardIDAndColumnPos =  `SELECT board_id, position FROM "column" WHERE id = $1`
+	QueryGetBoardIDAndColumnPos =  `
+		SELECT board_id, position 
+		FROM "column" 
+		WHERE id = $1`
 	
-	QueryMoveColumnsRight = `UPDATE "column"
+	QueryMoveColumnsRight = `
+		UPDATE "column"
 		SET position = position + 1
-		WHERE board_id = $1 AND position >= $2 AND position < $3`
+		WHERE board_id = $1 
+		AND position >= $2 
+		AND position < $3`
 
-	QueryMoveColumnsLeft = `UPDATE "column"
+	QueryMoveColumnsLeft = `
+		UPDATE "column"
 		SET position = position - 1
-		WHERE board_id = $1 AND position > $2 AND position <= $3`
+		WHERE board_id = $1 
+		AND position > $2 
+		AND position <= $3`
 
-	QueryUpdateColumn = `UPDATE "column"
+	QueryUpdateColumn = `
+		UPDATE "column"
 		SET name = COALESCE($1, name),
 			position = COALESCE($2, position),
 			updated_at = $3
@@ -102,68 +143,104 @@ const (
 
 	// Task queries
 
-	QueryGetTasksCount = `SELECT COUNT(*) FROM task WHERE column_id = $1`
+	QueryGetTasksCount = `
+		SELECT COUNT(*) 
+		FROM task 
+		WHERE column_id = $1`
 
-	QueryGetMaxTaskPosition = `SELECT COALESCE(MAX(position), 0) + 1 FROM task WHERE column_id = $1`
+	QueryGetMaxTaskPosition = `
+		SELECT COALESCE(MAX(position), 0) + 1 
+		FROM task 
+		WHERE column_id = $1`
 
-	QueryCreateTask = `INSERT INTO task
+	QueryCreateTask = `
+		INSERT INTO task
 		(id, column_id, created_at, updated_at, name, description, position, done)
 		VALUES ($1, $2, $3, $4, $5, $6, $7, $8)`
 	
-	QueryGetAllTasks = `SELECT * FROM task WHERE column_id = $1 ORDER BY position`
+	QueryGetAllTasks = `
+		SELECT * FROM task 
+		WHERE column_id = $1 
+		ORDER BY position`
 
-	QueryGetTask = `SELECT * FROM task WHERE id = $1`
+	QueryGetTask = `
+		SELECT * 
+		FROM task 
+		WHERE id = $1`
 
-	QueryUpdateTaskContent = `UPDATE task SET
-		name = COALESCE($1, name),
-		description = COALESCE($2, description),
-		done = COALESCE($3, done),
-		deadline = COALESCE($4, deadline),
-		updated_at = $5
+	QueryUpdateTaskContent = `
+		UPDATE task 
+		SET name = COALESCE($1, name),
+			description = COALESCE($2, description),
+			done = COALESCE($3, done),
+			deadline = COALESCE($4, deadline),
+			updated_at = $5
 		WHERE id = $6`
 	
-	QueryUpdateTaskColumn = `UPDATE task SET
-		column_id = $1,
-		position = $2,
-		updated_at = $3
+	QueryUpdateTaskColumn = `
+		UPDATE task 
+		SET column_id = $1,
+			position = $2,
+			updated_at = $3
 		WHERE id = $4`
 
-	QueryMoveTasksForInsert = `UPDATE task
+	QueryMoveTasksForInsert = `
+		UPDATE task
 		SET position = position + 1
 		WHERE column_id = $1
 		AND position >= $2`
 	
-	QueryMoveTaskForDelete = `UPDATE task
+	QueryMoveTaskForDelete = `
+		UPDATE task
 		SET position = position - 1
 		WHERE column_id = $1
 		AND position > $2`
 	
-	QueryGetColumnIDAndPosition = `SELECT column_id, position FROM task WHERE id = $1`
+	QueryGetColumnIDAndPosition = `
+		SELECT column_id, position 
+		FROM task 
+		WHERE id = $1`
 
-	QueryMoveTasksDown = `UPDATE task
+	QueryMoveTasksDown = `
+		UPDATE task
 		SET position = position + 1
-		WHERE column_id = $1 AND position >= $2 AND position < $3`
+		WHERE column_id = $1 
+		AND position >= $2 
+		AND position < $3`
 
-	QueryMoveTasksUp = `UPDATE task
+	QueryMoveTasksUp = `
+		UPDATE task
 		SET position = position - 1
-		WHERE column_id = $1 AND position > $2 AND position <= $3`
+		WHERE column_id = $1 
+		AND position > $2 
+		AND position <= $3`
 
-	QueryUpdateTaskPosition = `UPDATE task
-		SET position = $1, updated_at = $2
+	QueryUpdateTaskPosition = `
+		UPDATE task
+		SET position = $1, 
+			updated_at = $2
 		WHERE id = $3`
 
-	QueryDeleteTask = `DELETE FROM task WHERE id = $1`
+	QueryDeleteTask = `
+		DELETE FROM task 
+		WHERE id = $1`
 
 	// Queries for checking ownership
 
-	QueryGetUserByBoardID = `SELECT user_id FROM board WHERE id = $1`
+	QueryGetUserByBoardID = `
+		SELECT user_id 
+		FROM board 
+		WHERE id = $1`
 
-	QueryGetUserByColumnID = `SELECT board.user_id
+	QueryGetUserByColumnID = `
+		SELECT board.user_id
 		FROM board
-		JOIN "column" ON "column".board_id = board.id
+		JOIN "column"
+		ON "column".board_id = board.id
 		WHERE "column".id = $1`
 	
-	QueryGetUserByTaskID = `SELECT board.user_id
+	QueryGetUserByTaskID = `
+		SELECT board.user_id
 		FROM task
 		JOIN "column" ON task.column_id = "column".id
 		JOIN board ON "column".board_id = board.id
